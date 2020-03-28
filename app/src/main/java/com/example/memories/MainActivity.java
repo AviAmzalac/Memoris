887d0e66e7bcaee86e88dd6be15643fef5f7f41d
gregory caderby <bcaderby@gmail.com>
2020-03-28 17:23:44 +0400

    Signed-off-by: gregory caderby <bcaderby@gmail.com>
    ajout:
    	-layout : setting
    	-java : setting
    	-drawable :image drapeau fr et en

---
+++ app/src/main/java/com/example/memories/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.memories;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goto_difficulty_page(View view) {
+        Intent gameActivity = new Intent(MainActivity.this, difficulty_page.class);
+        startActivity(gameActivity);
+    }
+    public void goto_setting_page(View view) {
+        Intent gameActivity = new Intent(MainActivity.this, setting.class);
+        startActivity(gameActivity);
+    }
+}
---
+++ app/src/main/java/com/example/memories/setting.java
@@ -0,0 +1,19 @@
+package com.example.memories;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class setting extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setting);
+    }
+
+    public void goto_main(View view){
+        Intent gameActivity = new Intent(setting.this, MainActivity.class);
+        startActivity(gameActivity);
+    }
+}
