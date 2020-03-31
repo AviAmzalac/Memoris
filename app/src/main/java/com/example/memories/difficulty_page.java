d2817e34e8b5c9e1adf7012ee4f4699280977eb9
Khayros <avi.amzalac@gmail.com>
2020-03-31 11:13:38 +0400

    Signed-off-by: Khayros <avi.amzalac@gmail.com>
    Ajout du listener sur le bouton Ready
    Override de sa méthode onClick pour permettre plusieurs action
    Ajout du timer foncitonnel
    Ajout de la font SMB
    Pour passer a l'activity suivante mettre la méthode dans le onFinish()

--- app/src/main/java/com/example/memories/difficulty_page.java
+++ app/src/main/java/com/example/memories/difficulty_page.java
@@ -17,15 +17,10 @@ public class difficulty_page extends AppCompatActivity {
         Intent gameActivity = new Intent(difficulty_page.this, MainActivity.class);
         startActivity(gameActivity);
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
 
     public void goto_timer_page(View view){
         Intent gameActivity = new Intent(difficulty_page.this, timer_page.class);
         startActivity(gameActivity);
     }
-=======
->>>>>>> d77bac1d5bae5f60c8260a737aed7e1204f7e34f
-=======
->>>>>>> d77bac1d5bae5f60c8260a737aed7e1204f7e34f
+
 }
--- app/src/main/java/com/example/memories/timer_page.java
+++ app/src/main/java/com/example/memories/timer_page.java
@@ -1,38 +1,44 @@
 package com.example.memories;
 import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class timer_page extends AppCompatActivity {
+public class timer_page extends AppCompatActivity implements View.OnClickListener {
 
     private Button btnrdy;
     private TextView countdown;
+
+    CountDownTimer countDownTimer = new CountDownTimer(4 * 1000, 1000) {
+        @Override
+        public void onTick(long millisUntilFinished) {
+            countdown.setText("" + millisUntilFinished / 1000);
+        }
+
+        @Override
+        public void onFinish() {
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.timer_page);
         btnrdy = findViewById(R.id.btnrdy);
+        btnrdy.setOnClickListener(this);
         countdown = findViewById(R.id.countdown);
     }
 
-    public void make_btnrdy_disappear(View view) {
-         btnrdy.setVisibility(View.INVISIBLE);
-    }
-
-    public void start_countdown(View view) throws InterruptedException {
-        countdown.setText("3");
-        wait(1000);
-        countdown.setText("2");
-        wait(1000);
-        countdown.setText("1");
-        wait(1000);
-        countdown.setText("");
+    @Override
+    public void onClick(View v){
+        btnrdy.setVisibility(View.INVISIBLE);
+        countDownTimer.start();
     }
 }
+
 
 
 
