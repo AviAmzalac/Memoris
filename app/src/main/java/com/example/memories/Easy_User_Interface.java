3ffaf7ca8bf45d7f4e62d5e29c98ea4b30505cc5
Avi AMZALAC <avi.amzalac@gmail.com>
2020-04-25 20:39:31 +0400

    Signed-off-by: Gregory CADERBY <bcaderby@gmail.com>
    changement de la page easy_user et son fonctionnement
    changement des buttons en textview

--- app/src/main/java/com/example/memories/Easy_User_Interface.java
+++ app/src/main/java/com/example/memories/Easy_User_Interface.java
@@ -8,7 +8,7 @@ import android.os.Bundle;
 import android.os.IBinder;
 import android.os.PowerManager;
 import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
 import android.widget.TextView;
 
 
@@ -32,7 +32,6 @@ public class Easy_User_Interface extends AppCompatActivity {
         }
     };
 
-
     void doUnbindService() {
         if(mIsBound) {
             unbindService(Scon);
@@ -41,26 +40,27 @@ public class Easy_User_Interface extends AppCompatActivity {
     }
     //////////////////////////////////////////////////////////////////
 
-    static ArrayList<Button> List_button = new ArrayList<>();
+    static ArrayList<TextView> List_button = new ArrayList<>();
     static int score;
     private int nb_error =0;
     static int right_answer = 0;
-    private Button button_1;
-    private Button button_2;
-    private Button button_3;
+    private TextView text_1;
+    private TextView text_2;
+    private TextView text_3;
 
-    private Button button_4;
-    private Button button_5;
-    private Button button_6;
+    private TextView text_4;
+    private TextView text_5;
+    private TextView text_6;
 
-    private Button button_7;
-    private Button button_8;
-    private Button button_9;
+    private TextView text_7;
+    private TextView text_8;
+    private TextView text_9;
 
-    private Button button_10;
-    private Button button_11;
-    private Button button_12;
+    private TextView text_10;
+    private TextView text_11;
+    private TextView text_12;
     private TextView error_text;
+
 
 
     @Override
@@ -69,47 +69,48 @@ public class Easy_User_Interface extends AppCompatActivity {
 
         setContentView(R.layout.easy_user_interface);
         error_text = findViewById(R.id.error_counter);
-        button_1 = findViewById(R.id.button_1);
-        button_2 = findViewById(R.id.button_2);
-        button_3 = findViewById(R.id.button_3);
 
-        button_4 = findViewById(R.id.button_4);
-        button_5 = findViewById(R.id.button_5);
-        button_6 = findViewById(R.id.button_6);
+        text_1 = findViewById(R.id.text_1);
+        text_2 = findViewById(R.id.text_2);
+        text_3 = findViewById(R.id.text_3);
 
-        button_7 = findViewById(R.id.button_7);
-        button_8 = findViewById(R.id.button_8);
-        button_9 = findViewById(R.id.button_9);
+        text_4 = findViewById(R.id.text_4);
+        text_5 = findViewById(R.id.text_5);
+        text_6 = findViewById(R.id.text_6);
 
-        button_10 = findViewById(R.id.button_10);
-        button_11 = findViewById(R.id.button_11);
-        button_12 = findViewById(R.id.button_12);
+        text_7 = findViewById(R.id.text_7);
+        text_8 = findViewById(R.id.text_8);
+        text_9 = findViewById(R.id.text_9);
 
-        List_button.add(button_1);
-        List_button.add(button_2);
-        List_button.add(button_3);
+        text_10 = findViewById(R.id.text_10);
+        text_11 = findViewById(R.id.text_11);
+        text_12 = findViewById(R.id.text_12);
 
-        List_button.add(button_4);
-        List_button.add(button_5);
-        List_button.add(button_6);
+        List_button.add(text_1);
+        List_button.add(text_2);
+        List_button.add(text_3);
 
-        List_button.add(button_7);
-        List_button.add(button_8);
-        List_button.add(button_9);
+        List_button.add(text_4);
+        List_button.add(text_5);
+        List_button.add(text_6);
 
-        List_button.add(button_10);
-        List_button.add(button_11);
-        List_button.add(button_12);
+        List_button.add(text_7);
+        List_button.add(text_8);
+        List_button.add(text_9);
+
+        List_button.add(text_10);
+        List_button.add(text_11);
+        List_button.add(text_12);
 
 
-        System.out.println("+++++++++++++++ LIST AFFICHE"+Jeu.List_affiche);
-        System.out.println("+++++++++++++++ LIST REP"+Jeu.List_reponse);
-        Integer text1 = Jeu.List_affiche.get(0);
+        System.out.println("+++++++++++++++ LIST AFFICHE"+ com.example.memories.Jeu.List_affiche);
+        System.out.println("+++++++++++++++ LIST REP"+ com.example.memories.Jeu.List_reponse);
+        Integer text1 = com.example.memories.Jeu.List_affiche.get(0);
         System.out.println(getResources().getString(text1));
         afficher_list();
 
-        for (int i = 0; i <= Jeu.List_affiche.size()-1; i++) {
-            Integer text = Jeu.List_affiche.get(i);
+        for (int i = 0; i <= com.example.memories.Jeu.List_affiche.size()-1; i++) {
+            Integer text = com.example.memories.Jeu.List_affiche.get(i);
             List_button.get(i).setText(getResources().getString(text));
 
         }
@@ -132,8 +133,10 @@ public class Easy_User_Interface extends AppCompatActivity {
         mHomeWatcher.startWatch();
 
 
+
     }
 ///////////////////////////////////////////// FIN ONCREATE
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -179,8 +182,7 @@ public class Easy_User_Interface extends AppCompatActivity {
 
     public void goto_leaderboard(){
         Intent intent = new Intent(Easy_User_Interface.this, Results_page.class);
-        intent.putExtra("Ranswer", right_answer);
-        intent.putExtra("Difficulty","Easy");
+        intent.putExtra("SCORE", score);
         startActivity(intent);
     }
 
@@ -192,55 +194,55 @@ public class Easy_User_Interface extends AppCompatActivity {
     public void push_button(View view){
 
             switch (view.getId()) {
-                case R.id.button_1:
-                    button_1.setVisibility(View.INVISIBLE);
+                case R.id.text_1:
+                    text_1.setVisibility(View.INVISIBLE);
                     verification(view);
                     break;
-                case R.id.button_2:
-                    button_2.setVisibility(View.INVISIBLE);
+                case R.id.text_2:
+                    text_2.setVisibility(View.INVISIBLE);
                     verification(view);
                     break;
-                case R.id.button_3:
-                    button_3.setVisibility(View.INVISIBLE);
-                    verification(view);
-                    break;
-
-                case R.id.button_4:
-                    button_4.setVisibility(View.INVISIBLE);
-                    verification(view);
-                    break;
-                case R.id.button_5:
-                    button_5.setVisibility(View.INVISIBLE);
-                    verification(view);
-                    break;
-                case R.id.button_6:
-                    button_6.setVisibility(View.INVISIBLE);
+                case R.id.text_3:
+                    text_3.setVisibility(View.INVISIBLE);
                     verification(view);
                     break;
 
-                case R.id.button_7:
-                    button_7.setVisibility(View.INVISIBLE);
+                case R.id.text_4:
+                    text_4.setVisibility(View.INVISIBLE);
                     verification(view);
                     break;
-                case R.id.button_8:
-                    button_8.setVisibility(View.INVISIBLE);
+                case R.id.text_5:
+                    text_5.setVisibility(View.INVISIBLE);
                     verification(view);
                     break;
-                case R.id.button_9:
-                    button_9.setVisibility(View.INVISIBLE);
+                case R.id.text_6:
+                    text_6.setVisibility(View.INVISIBLE);
                     verification(view);
                     break;
 
-                case R.id.button_10:
-                    button_10.setVisibility(View.INVISIBLE);
+                case R.id.text_7:
+                    text_7.setVisibility(View.INVISIBLE);
                     verification(view);
                     break;
-                case R.id.button_11:
-                    button_11.setVisibility(View.INVISIBLE);
+                case R.id.text_8:
+                    text_8.setVisibility(View.INVISIBLE);
                     verification(view);
                     break;
-                case R.id.button_12:
-                    button_12.setVisibility(View.INVISIBLE);
+                case R.id.text_9:
+                    text_9.setVisibility(View.INVISIBLE);
+                    verification(view);
+                    break;
+
+                case R.id.text_10:
+                    text_10.setVisibility(View.INVISIBLE);
+                    verification(view);
+                    break;
+                case R.id.text_11:
+                    text_11.setVisibility(View.INVISIBLE);
+                    verification(view);
+                    break;
+                case R.id.text_12:
+                    text_12.setVisibility(View.INVISIBLE);
                     verification(view);
                     break;
                 default:
@@ -252,19 +254,21 @@ public class Easy_User_Interface extends AppCompatActivity {
         int flag = 0;
         int i=0;
 
-        Button b = (Button)view;
+        TextView b = (TextView)view;
         String buttonText = b.getText().toString();
         System.out.println(" NOM DU TEXT DU BUTOTN : "+buttonText);
 
         //        System.out.println(getResources().getString(text1));
-        while(flag==0 && i < Jeu.List_reponse.size()){
-            int text1 = Jeu.List_reponse.get(i);
+        while(flag==0 && i < com.example.memories.Jeu.List_reponse.size()){
+
+            int text1 = com.example.memories.Jeu.List_reponse.get(i);
 
             if(buttonText.equals(getResources().getString(text1))){
                 System.out.println( getResources().getString(text1)+" OK");
                 flag = 1;
-                right_answer++;
-            } else{
+
+                right_answer++;}
+            else{
 
                 System.out.println( getResources().getString(text1)+" Faux");}
                  i++;
@@ -284,16 +288,17 @@ public class Easy_User_Interface extends AppCompatActivity {
         }
     }
     public void afficher_list(){
-        for(int i = 0; i< Jeu.List_affiche.size();i++){
-            int text1 = Jeu.List_affiche.get(i);
+        for(int i = 0; i< com.example.memories.Jeu.List_affiche.size(); i++){
+            int text1 = com.example.memories.Jeu.List_affiche.get(i);
 
             System.out.print( getResources().getString(text1)+", ");
         }
         System.out.println("-");
-        for(int j = 0; j< Jeu.List_reponse.size();j++){
-            int text2 = Jeu.List_reponse.get(j);
+        for(int j = 0; j< com.example.memories.Jeu.List_reponse.size(); j++){
+            int text2 = com.example.memories.Jeu.List_reponse.get(j);
             System.out.print( getResources().getString(text2)+", ");
         }
         System.out.println("-");
     }
+
 }
